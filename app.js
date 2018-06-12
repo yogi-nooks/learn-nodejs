8c6c9dff9f642e9e7e1ee35c111023bdc53e2a90
yogesh-nooks <yogesh.nooks@gmail.com>
2018-06-12 13:37:06 +0530

    add about route in app.js file

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req , res) =>{
     res.send('Hoola Hoop hooo hoo');
 })
 
+app.get('/about', (req , res) =>{
+    res.send('About Hoola');
+})
+
 const port  = 5000;
 
 app.listen(port, () =>{
