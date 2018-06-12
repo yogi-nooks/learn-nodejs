09dbee12bdf44e6b32b9133c62d1f4c47b7d2c7f
yogesh-nooks <yogesh.nooks@gmail.com>
2018-06-12 16:38:50 +0530

    chngs in learning moudle 02-013-working-with-templates nodejs

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
+//Middle ware
+app.use(function(req, res, next){
+    console.log(Date.now());
+    next();
+})
+
 //Creating route
 
-app.get('/', (req , res) =>{
-    res.send('Hoola Hoop hooo hoo');
+app.get('/', (req , res, next) =>{
+    req.name = 'yogesh';
+    res.send(req.name);
+    next();
+    
+
 })
 
 app.get('/about', (req , res) =>{
-    res.send('About Hoola');
+    res.send('About Hoola is there any thing else ');
 })
 
 const port  = 5000;
